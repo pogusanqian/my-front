ec57cde6e105737aaeee84779113588ee2598f5b
pogusanqian <pogusanqian@163.com>
2023-02-23 21:09:58 +0800

    按需引入element

--- T05-Vue/vue3-admin/src/App.vue
+++ T05-Vue/vue3-admin/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <el-button type="primary">Primary</el-button>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
--- T05-Vue/vue3-admin/vue.config.js
+++ T05-Vue/vue3-admin/vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    // 按需引入elementUI, 会自动生成auto-imports.d.ts和components.d.ts两个声明文件
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
 })
